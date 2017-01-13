8d5bfe664eaf15203205d3ead9fe43943909d4a7
Kenny Grant <kennygrant@gmail.com>
2017-01-13 14:00:02 +0000

    Adjusted test command again to fix broken args

--- test.go
+++ test.go
@@ -3,23 +3,23 @@ package main
 import (
 	"log"
 	"path/filepath"
+	"strings"
 )
 
 // RunTests runs all tests below the current path or the path specified
 // this is a simplistic wrapper around the go test tool
 func RunTests(args []string) {
 
-	// Send two paths to go - root and src
-	// we do this to ignore the vendor dir
-	testDirs := filepath.Join(". .", "src", "...")
+	// Run tests on the src dir, this skips root server tests but also skips vendor tests
+	testDir := strings.Join([]string{".", "src", "..."}, string(filepath.Separator))
 
 	if len(args) > 0 {
-		testDirs = args[0]
+		testDir = args[0]
 	}
 
-	log.Printf("Running tests at %s", testDirs)
+	log.Printf("Running tests at %s", testDir)
 
-	result, err := runCommand("go", "test", testDirs)
+	result, err := runCommand("go", "test", testDir)
 	if err != nil {
 		log.Printf("Error running tests %s", err)
 	}
